14885e6e9cb2411c764356e26d991c1a57656a78
vivekanandpv-mac <mail@vivekanandpv.com>
2019-11-25 16:19:37 +0530

    Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PageContainerComponent } from './page-container/page-container.component';
+import { ItemCardComponent } from './item-card/item-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PageContainerComponent,
+    ItemCardComponent
   ],
   imports: [
     BrowserModule
